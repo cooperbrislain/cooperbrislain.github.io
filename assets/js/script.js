93770cc42408103bbbf377821135ce410e12b54a
cooperbrislain <cbrislain@gmail.com>
2021-05-09 21:18:55 -0700

    getting started on javascript fun

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,12 @@ console.log('script');
 $(() => {
     console.log("ready!");
 
-    $(document).on('mouseOver', 'h4', e => {
+    $(document).on('click', 'h4 span', e => {
         console.log(e.target);
+        if($(e.target).is('.selected')) {
+            $(e.target).removeClass('selected');
+        } else {
+            $(e.target).addClass('selected');
+        }
     });
 });
